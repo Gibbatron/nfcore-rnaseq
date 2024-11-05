1c1597c1bcfdfc3cf5f31db5a63bdac1caeb5244
heathbioi <142903078+heathbioi@users.noreply.github.com>
2024-11-05 14:54:22 +0000

    updates script

--- bin/pipeline.sh
+++ bin/pipeline.sh
@@ -8,14 +8,14 @@ nextflow run nf-core/fetchngs -r dev -profile singularity -c resources/my.config
 #############
 #download reference genome
 #download the human GRCh38 reference DNA sequence
+cd resources/
 wget https://ftp.ensembl.org/pub/release-110/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz 
 gunzip Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz
-mv Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz resources/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz
 
 #download the human GRCh38 reference gene information file
 wget https://ftp.ensembl.org/pub/release-110/gtf/homo_sapiens/Homo_sapiens.GRCh38.110.gtf.gz 
 gunzip Homo_sapiens.GRCh38.110.gtf.gz
-mv Homo_sapiens.GRCh38.110.gtf.gz resources/Homo_sapiens.GRCh38.110.gtf.gz
+cd ../
 #############
 
 #############
