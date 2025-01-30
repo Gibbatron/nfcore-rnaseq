601daf935b13ef2e3629e70f52dfd1f172146975
Alex Gibbs <aw_gibbs@aol.com>
2025-01-30 10:00:32 +0000

    Update rnaseq.sh

--- bin/rnaseq.sh
+++ bin/rnaseq.sh
@@ -5,11 +5,11 @@
 #download the human GRCh38 reference DNA sequence
 cd resources/
 wget https://ftp.ensembl.org/pub/release-110/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz 
-gunzip Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz
+gunzip -f Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz
 
 #download the human GRCh38 reference gene information file
 wget https://ftp.ensembl.org/pub/release-110/gtf/homo_sapiens/Homo_sapiens.GRCh38.110.gtf.gz 
-gunzip Homo_sapiens.GRCh38.110.gtf.gz
+gunzip -f Homo_sapiens.GRCh38.110.gtf.gz
 cd ../
 #############
 
